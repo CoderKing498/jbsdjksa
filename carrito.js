b046b209d3f8c8fbd04ae9ea74439bb58020456c
CoderKing498 <yerlenisarrieta@gmail.com>
2026-05-19 19:51:07 -0500

    Improve product cart

--- carrito.js
+++ carrito.js
@@ -6,18 +6,50 @@ const products = [
     {ID: 4, name: 'Product4', price:160000, quantity: 5 }
 ]
 
-let carrito = []
+let carrito = [];
 
-function addToCard(product){
+function addToCard(product) {
     carrito.push(product);
 
-    const containerTotal = document.getElementById("container-total")
+    const containerTotal = document.getElementById("container-total");
 
-    const div = document.createElement("div")
-    div.className = "product-aggregate"
+    const div = document.createElement("div");
+    div.className = "product-aggregate";
 
     div.innerHTML = `
-    <h3>${product.name}
-    `
+        <h3>${product.name}</h3>
+        <p>Price: ${product.price}</p>
+        <p>Quantity: ${product.quantity}</p>
+    `;
+
+    containerTotal.appendChild(div);
+    getTotal();
 }
 
+function getTotal() {
+    let total = 0;
+    carrito.forEach(product => total += product.price * product.quantity);
+    const totalElement = document.getElementById("total");
+    totalElement.textContent = `Total: $${total}`;
+    return total;
+}
+
+for (const product of products) {
+    const div = document.createElement("div");
+    const container = document.getElementById("container");
+
+    div.className = "product-item";
+    div.innerHTML = `
+        <h3>${product.name}</h3>
+        <p>Price: ${product.price}</p>
+        <p>Quantity: ${product.quantity}</p>
+        <button >Add to Cart</button>
+    `;
+
+    const button = div.querySelector("button");
+    button.addEventListener("click", function () {
+        addToCard(product);
+    });
+
+    container.appendChild(div);
+}
